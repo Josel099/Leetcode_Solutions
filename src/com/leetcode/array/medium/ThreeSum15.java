e448e74ef1e3b656e1ecac1ca15afe96c02c269e
Josel Xavier <joseltech2002@gmail.com>
2023-09-20 23:59:05 +0530

    3 sum

---
+++ src/com/leetcode/array/medium/ThreeSum15.java
@@ -0,0 +1,17 @@
+package com.leetcode.array.medium;
+
+import java.util.List;
+
+public class ThreeSum15 {
+}
+
+ class Solution15{
+     public List<List<Integer>> threeSum(int[] nums) {
+
+
+
+
+
+     }
+
+ }
