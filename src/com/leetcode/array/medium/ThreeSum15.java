9d585b6da6c81230f01cad5d4b3a41fd3432abbb
Josel Xavier <joseltech2002@gmail.com>
2023-09-25 22:57:51 +0530

    3 sum done 25-09-2023.

--- src/com/leetcode/array/medium/ThreeSum15.java
+++ src/com/leetcode/array/medium/ThreeSum15.java
@@ -11,7 +11,7 @@ public class ThreeSum15 {
      public List<List<Integer>> threeSum(int[] nums) {
          List<List<Integer>> result = new ArrayList<>();
          Arrays.sort(nums);
-         for(int i = 0 ; i< nums.length ; i++){
+         for(int i = 0 ; i< nums.length-2 ; i++){
              if( i > 0 && nums[i] == nums[i-1]) continue; // to skip the duplicate of first element of the triplet
            int  j=i+1;
            int k = nums.length-1;
@@ -21,12 +21,13 @@ public class ThreeSum15 {
                 else if (sum > 0) k--;
                 else if (sum == 0 ) {
                     result.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    // Skip duplicates for the second and third elements of the triplet.
+                    while( j < k && nums[j] == nums[j+1]) j++ ;
+                    while(j < k && nums[k] == nums[k-1]) k-- ;
                     j++;
                     k--;
                 }
-                // Skip duplicates for the second and third elements of the triplet.
-                while (j < nums.length - 1 && nums[j] == nums[j + 1]) j++;
-                while (k >= i + 1 && nums[k] == nums[k - 1]) k--;
+
             }
 
          }
