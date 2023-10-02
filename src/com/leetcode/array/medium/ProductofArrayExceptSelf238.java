6daee2337e65e5f00ca6ab03551160b00ed31a43
Josel Xavier <joseltech2002@gmail.com>
2023-10-02 13:49:13 +0530

    ProductofArrayExceptSelf 238  done with O(n) time and O(1) space complexities

--- src/com/leetcode/array/medium/ProductofArrayExceptSelf238.java
+++ src/com/leetcode/array/medium/ProductofArrayExceptSelf238.java
@@ -1,2 +1,32 @@
-package com.leetcode.array.medium;public class ProductofArrayExceptSelf238 {
+package com.leetcode.array.medium;
+
+public class  ProductofArrayExceptSelf238 {
+
+}
+class Solution238 {
+    public int[] productExceptSelf(int[] nums) {
+
+        int[] Answer = new int[nums.length];
+
+        //int prefix = 1 ;
+        for(int i = 0 ; i< nums.length ; i++){
+
+            if(i==0) Answer[i] = 1;
+            else{
+                Answer[i] = Answer[i-1] * nums[i-1] ;
+            }
+        }
+
+        int postfix = 1 ;
+        for(int i = nums.length-1 ; i>=0 ; i--){
+
+            if(i == nums.length-1) Answer[i] = Answer[i] * postfix ;
+            else{
+                postfix = postfix* nums[i+1] ;
+                Answer[i] = Answer[i]*postfix ;
+            }
+        }
+
+    return  Answer;
+    }
 }
