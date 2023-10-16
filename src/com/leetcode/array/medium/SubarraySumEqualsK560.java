102e5fe956ebd769832c01378f693d22847286f2
Josel Xavier <joseltech2002@gmail.com>
2023-10-16 23:10:07 +0530

    subarray sum equals k 560 : bruteforce approach done

--- src/com/leetcode/array/medium/SubarraySumEqualsK560.java
+++ src/com/leetcode/array/medium/SubarraySumEqualsK560.java
@@ -1,2 +1,29 @@
-package com.leetcode.array.medium;public class SubarraySumEqualsK560 {
+package com.leetcode.array.medium;
+
+public class SubarraySumEqualsK560 {
+
+}
+class Solution560 {  // bruteforce approach
+    public int subarraySum(int[] nums, int k) {
+        int count=0 ;
+        for(int i = 0 ; i< nums.length ; i++ ){ // loop for first position moving
+             int sum = nums[i] ;
+             if(sum == k ) {                    // condition for find  if an one element of the array is equals to the k (target element);
+                 count++;
+             }
+             for(int j=i+1 ; j< nums.length; j++){    // loop for continuous sub array sum finding
+                 sum = sum+ nums[j];
+
+                 if(sum == k ) count++;
+
+             }
+        }
+ return count;
+    }
+}
+
+class Solution5602{
+    public int subarraySum(int[] nums , int k ) {
+
+    }
 }
