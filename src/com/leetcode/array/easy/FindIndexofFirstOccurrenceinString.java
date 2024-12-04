461cfc2d9e38144dda1b8c5497b8958790606bad
Josel Xavier_m <josel.x@livares.com>
2024-12-04 13:05:58 +0530

    2 problem solved

---
+++ src/com/leetcode/array/easy/FindIndexofFirstOccurrenceinString.java
@@ -0,0 +1,12 @@
+package com.leetcode.array.easy;
+
+public class FindIndexofFirstOccurrenceinString {
+
+    public static void main(String[] args) {
+
+    }
+
+
+    public int strStr(String haystack, String needle) {
+            return haystack.indexOf(needle);
+    }}
